145d1c96ec3f6ff58b24866b3f3e3170c9900096
hugo juhel <juhel.hugo@stratemia.com>
2023-10-05 18:49:29 -0400

    feat: computing a yearly (with years being expanded) version of the table

---
+++ models/marts/human_resources/features/fact_activity_yearly.sql
@@ -0,0 +1,163 @@
+{#
+Dashboards Store - Helping students, one dashboard at a time.
+Copyright (C) 2023  Sciance Inc.
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU Affero General Public License as
+published by the Free Software Foundation, either version 3 of the
+License, or any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Affero General Public License for more details.
+
+You should have received a copy of the GNU Affero General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
+#}
+{# 
+    Compute a yearly version of the employment history restricted to only active status
+#}
+-- Fetch the ACTIVE employment history
+with
+    histo as (
+        select
+            hst.matr,
+            hst.school_year,
+            hst.ref_empl,
+            hst.corp_empl,
+            hst.etat_empl,
+            hst.lieu_trav,
+            hst.date_eff,
+            hst.date_fin
+        from {{ ref("stg_employment_history") }} as hst
+        inner join
+            {{ ref("dim_etat_empl_yearly") }} as dm
+            on hst.school_year = dm.school_year
+            and hst.etat_empl = dm.etat_empl
+            and dm.etat_actif = 1
+        where hst.school_year > {{ store.get_current_year() }} - 10  -- Inferring the main job before that is shady at best
+
+    -- Yearly padd the active history 
+    ),
+    padded as (
+        select
+            matr,
+            school_year + seq.seq_value as school_year,
+            ref_empl,
+            corp_empl,
+            etat_empl,
+            lieu_trav,
+            date_eff,
+            date_fin
+        from histo
+        cross join
+            (
+                select seq_value
+                from {{ ref("int_sequence_0_to_1000") }}
+                where seq_value <= 50
+            ) as seq
+        where
+            case
+                when month(date_fin) between 9 and 12
+                then year(date_fin)
+                else year(date_fin) - 1
+            end
+            >= (school_year + seq.seq_value)
+
+    -- Only keep the latest version per year
+    ),
+    up_to_date as (
+        select
+            base.matr,
+            base.school_year,
+            base.ref_empl,
+            base.corp_empl,
+            base.etat_empl,
+            base.lieu_trav,
+            base.date_eff,
+            base.date_fin
+        from
+            (
+                select
+                    matr,
+                    school_year,
+                    ref_empl,
+                    corp_empl,
+                    etat_empl,
+                    lieu_trav,
+                    date_eff,
+                    date_fin,
+                    row_number() over (
+                        partition by matr, school_year, ref_empl order by date_eff desc
+                    ) as seq_id
+                from padded
+            ) as base
+        where base.seq_id = 1
+
+    -- add the main job ref
+    ),
+    main_job as (
+        select
+            src.matr,
+            src.school_year,
+            src.ref_empl,
+            src.corp_empl,
+            src.etat_empl,
+            src.lieu_trav,
+            src.date_eff,
+            src.date_fin,
+            case when mn.main_job_ref is null then 0 else 1 end as is_main_job
+        from up_to_date as src
+        left join
+            {{ ref("stg_main_job_transition") }} as mn
+            on src.matr = mn.matr
+            and src.school_year = mn.school_year
+            and src.ref_empl = mn.main_job_ref
+
+    -- Adding the main job log identifies the main job in about 85% of the cases.
+    -- The remaining 15% are identified by the following logic:
+    -- 1. If the employee has only one job, then it is the main job -> account for
+    -- about 3% of the cases
+    -- 2. If the employee has more than one job, then i'm fucked -_-
+    ),
+    patched as (
+        select
+            matr,
+            school_year,
+            ref_empl,
+            corp_empl,
+            etat_empl,
+            lieu_trav,
+            date_eff,
+            date_fin,
+            is_main_job,
+            -- count distinct is not allowed in a windows function, so I first compute
+            -- a dense rank and then max it
+            dense_rank() over (
+                partition by matr, school_year order by ref_empl
+            ) as rank_
+        from main_job
+    )
+
+select
+    matr,
+    school_year,
+    ref_empl,
+    corp_empl,
+    etat_empl,
+    lieu_trav,
+    date_eff,
+    date_fin,
+    case
+        when
+            max(rank_) over (
+                partition by matr, school_year
+                order by date_eff
+                rows between unbounded preceding and unbounded following
+            )
+            = 1
+        then 1
+        else is_main_job
+    end as is_main_job
+from patched
