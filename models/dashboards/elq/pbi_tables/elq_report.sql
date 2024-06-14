3e410ae805fa7b51cb4cb41281f9c0bcefff552b
Gabriel Thiffault <thiffaultg@csvt.qc.ca>
2024-06-14 13:51:18 -0400

    feat: dashboard completed

--- models/dashboards/elq/pbi_tables/elq_report.sql
+++ models/dashboards/elq/pbi_tables/elq_report.sql
@@ -17,52 +17,54 @@ You should have received a copy of the GNU Affero General Public License
 along with this program.  If not, see <https://www.gnu.org/licenses/>.
 #}
 {{ config(alias="elq_report") }}
+
+
 select
     ensq.matr as matricule,
-    ensq.birth_date,
+    emp.legal_name as 'nom_legal',
+    emp.sex_friendly_name as genre,
     case
-        when datediff(year, ensq.birth_date, getdate()) < 25
+        when datediff(year, emp.birth_date, getdate()) < 25
         then '24 ans et moins'
         when
-            datediff(year, ensq.birth_date, getdate()) >= 25
-            and datediff(year, ensq.birth_date, getdate()) < 35
+            datediff(year, emp.birth_date, getdate()) >= 25
+            and datediff(year, emp.birth_date, getdate()) < 35
         then '25 à 34 ans'
         when
-            datediff(year, ensq.birth_date, getdate()) >= 35
-            and datediff(year, ensq.birth_date, getdate()) < 45
+            datediff(year, emp.birth_date, getdate()) >= 35
+            and datediff(year, emp.birth_date, getdate()) < 45
         then '35 à 44 ans'
         when
-            datediff(year, ensq.birth_date, getdate()) >= 45
-            and datediff(year, ensq.birth_date, getdate()) < 55
+            datediff(year, emp.birth_date, getdate()) >= 45
+            and datediff(year, emp.birth_date, getdate()) < 55
         then '45 à 54 ans'
         when
-            datediff(year, ensq.birth_date, getdate()) >= 55
-            and datediff(year, ensq.birth_date, getdate()) < 65
+            datediff(year, emp.birth_date, getdate()) >= 55
+            and datediff(year, emp.birth_date, getdate()) < 65
         then '55 à 64 ans'
-        when datediff(year, ensq.birth_date, getdate()) >= 65
+        when datediff(year, emp.birth_date, getdate()) >= 65
         then '65 ans et plus'
     end as 'tranche_age',
-    ensq.genre,
     lieu.workplace_name as lieu_principal,
     job_class.code_job_name as corp_empl,
     stat.stateng_name as stat_eng,
-    case
-        when qualif.code is null then 'Aucune' else qualif.code + ' - ' + qualif.descr
-    end as qualification,
-    qualif.is_qualified 'elq',
+    case when qualif.code is null then 'Aucune' else qualif.descr end as qualification,
     state.descr as etat_empl,
-    case when genre = 'femme' then 1 end as total_femme,
-    case when genre = 'homme' then 1 end as total_homme,
+    --
+    case when emp.sex_friendly_name = 'femme' then 1 end as total_femme,
+    case when emp.sex_friendly_name = 'homme' then 1 end as total_homme,
     case when qualif.is_qualified = 1 then 1 end as total_elq,
-    case when genre = 'femme' or genre = 'homme' then 1 end as total_ens
+    case when ensq.matr is not null then 1 end as total_ens
 
 from {{ ref("fact_ens_qualif") }} as ensq
 
-left join {{ ref("dim_mapper_workplace") }} lieu on ensq.workplace = lieu.workplace
-left join {{ ref("etat_empl") }} state on ensq.etat_empl = state.etat_empl
+inner join {{ ref("dim_employees") }} as emp on ensq.matr = emp.matr
+inner join {{ ref("dim_mapper_workplace") }} lieu on ensq.workplace = lieu.workplace
+inner join {{ ref("etat_empl") }} state on ensq.etat_empl = state.etat_empl
 inner join
     {{ ref("dim_mapper_job_class") }} as job_class
     on job_class.code_job = ensq.corp_empl
 inner join {{ ref("dim_mapper_stat_eng") }} stat on ensq.stat_eng = stat.stat_eng
-left join  -- Requis pour aller chercher uniquement les codes concernés
-    {{ ref("ens_qualification") }} qualif on ensq.type_qualif = qualif.code
+
+-- Requis pour aller chercher uniquement les codes concernés	
+left join {{ ref("ens_qualification") }} qualif on ensq.type_qualif = qualif.code
--- models/marts/human_resources/features/elq/fact_ens_qualif.sql
+++ models/marts/human_resources/features/elq/fact_ens_qualif.sql
@@ -1,20 +1,22 @@
 -- fact_ens_qualif.sql
 select
     util.matr,
-    util.birth_date,
     emp.etat as etat_empl,
     emp.lieu_trav as workplace,
     emp.stat_eng,
     emp.type,
-    util.sex_friendly_name as genre,
     emp.corp_empl,
-    emp.mode_cour,
     ca.emp_actif,
     qa.type_qualif
 from {{ ref("dim_employees") }} as util
-left join {{ ref("i_pai_dos_empl") }} as emp on util.matr = emp.matr
-left join {{ ref("fact_activity_current") }} as ca on util.matr = ca.matr
-left join {{ ref("etat_empl") }} state on emp.etat = state.etat_empl
-left join {{ ref("i_pai_qualif") }} as qa on util.matr = qa.matr
+inner join {{ ref("i_pai_dos_empl") }} as emp on util.matr = emp.matr
+inner join {{ ref("etat_empl") }} state on emp.etat = state.etat_empl
 
-where state.etat_actif = 1 and emp.ind_empl_princ = 1 and emp.corp_empl like '3%'
+-- LEFT JOIN requis pour assurer une bonne représentation de la population
+left join {{ ref("i_pai_qualif") }} as qa on util.matr = qa.matr
+left join {{ ref("fact_activity_current") }} as ca on util.matr = ca.matr
+where
+    state.etat_actif = 1
+    and emp.ind_empl_princ = 1
+    and ca.emp_actif = 1
+    and emp.corp_empl like '3%'
